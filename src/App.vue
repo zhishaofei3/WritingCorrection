37507b54f41dbd58f3870e451e9f668cfeca2050
zhishaofei <zhishaofei@vipkid.com.cn>
2018-06-20 20:53:31 +0800

    缩放 旋转 重置

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <div class="writing-wrap">
         <img src="./assets/kuan.jpg" alt="" style="display: none;">
         <img src="./assets/shu.jpg" alt="" style="display: none;">
+        <img src="./assets/QQ20180619-122931.png" alt="" style="display: none;">
         <div class="img-wrap">
           <canvas id="myCanvas" width="1000" height="750"></canvas>
         </div>
@@ -13,8 +14,8 @@
           <a href="#none" target="_self" @click="onClickEraserBtn">橡皮</a>
           <a href="#none" target="_self" @click="onClickRotationBtn">旋转</a>
           <a href="#none" target="_self" @click="onClickResetBtn">重置</a>
-          <a href="#none" target="_self" @click="onClickBigBtn">放大</a>
-          <a href="#none" target="_self" @click="onClickSmallBtn">缩小</a>
+          <a href="#none" target="_self" @click="onClickZoomInBtn">放大</a>
+          <a href="#none" target="_self" @click="onClickZoomOutBtn">缩小</a>
           <a href="#none" target="_self" @click="onClickPrevBtn">上一页</a>
           <span>1/3</span>
           <a href="#" target="#none" @click="onClickNextBtn">下一页</a>
@@ -28,6 +29,7 @@
 <script>
 import {fabric} from 'fabric'
 import mode from './mode'
+import {getPathStr} from './tools'
 export default {
   name: 'app',
   data() {
@@ -35,30 +37,76 @@ export default {
       imgW: 0,
       imgH: 0,
       mode: mode.MOVE,
-      rotation: 0, // 0 90 180 270 360
+      rotation: 0, // 0 90 180 270 360,
+      group: null,
+      fabricImg: null, // fabric.Image
+      zoomArr: [1, 1.15, 1.3 , 1.45, 1.6, 1.75, 1.9, 2.05, 2.2],
+      zoomNum: 0,
     }
   },
   mounted() {
     this.myCanvas = new fabric.Canvas('myCanvas')
-    var fabricImg = null
+    this.myCanvas.selection = false
     var img = new Image()
     // img.src = 'http://test-ic-static.vipkid.com.cn/course/material/DEMO1-U1-LC1-L2/5827cbca05bdb0dcdf20b8d6b261ec34.jpg'
     // img.src = './dist/kuan.jpg'
     img.src = './dist/shu.jpg'
+    // img.src = './dist/QQ20180619-122931.png'
     img.crossOrigin = 'anonymous'
     img.addEventListener('load', () => {
       console.log('img wh', img.width, img.height)
-      fabricImg = new fabric.Image(img, {width: img.width, height: img.height, crossOrigin: 'anonymous'})
-      fabricImg.crossOrigin = 'anonymous'
-      this.myCanvas.add(fabricImg)
+      this.fabricImg = new fabric.Image(img, {width: img.width, height: img.height, crossOrigin: 'anonymous'})
+      this.fabricImg.crossOrigin = 'anonymous'
       let suoFangBiLi = this.coumputeWH(img.width, img.height)
       console.log('zsf suoFangBiLi', suoFangBiLi)
-      fabricImg.scaleX = suoFangBiLi
-      fabricImg.scaleY = suoFangBiLi
+      this.fabricImg.scaleX = suoFangBiLi
+      this.fabricImg.scaleY = suoFangBiLi
+      this.fabricImg.hoverCursor = 'default'
+      this.fabricImg.selectable = false
+      this.fabricImg.hasBorders = false
+      this.fabricImg.hasControls = false
+      this.myCanvas.add(this.fabricImg)
       this.myCanvas.renderAll()
     })
+    this.addEvents()
   },
   methods: {
+    addEvents() {
+      this.myCanvas.on('path:created', opt => {
+        var shape = opt.path
+        this.myCanvas.remove(shape)
+        var path = new fabric.Path(getPathStr(shape), {
+          strokeLineCap: 'round',
+          strokeLineJoin: 'round',
+          fill: null,
+          stroke: shape.stroke,
+          strokeWidth: shape.strokeWidth,
+          hasControls: false,
+          hasBorders: false,
+          selectable: false,
+        })
+        this.myCanvas.add(path)
+      })
+
+      this.myCanvas.on('selection:cleared', opt => { // 移动模式不能取消全部选区激活状态 加上边界判断后可以删除
+        if (this.mode == mode.MOVE) {
+          this.onClickMoveBtn()
+        }
+      })
+
+      this.myCanvas.on('mouse:wheel', opt => { // 放大后有边框大小没更新问题 待修复
+        let curZoom = this.myCanvas.getZoom()
+        curZoom += opt.e.deltaY < 0 ? 0.15 : -0.15
+        if (curZoom > 2.2) {
+          curZoom = 2.2
+        } else if (curZoom < 1) {
+          curZoom = 1
+        }
+        this.myCanvas.zoomToPoint(new fabric.Point(opt.e.offsetX, opt.e.offsetY), curZoom)
+        opt.e.preventDefault()
+        opt.e.stopPropagation()
+      })
+    },
     coumputeWH(sw, sh) {
       let sourceBili = sh / sw
       let w = 0, h = 0
@@ -71,35 +119,125 @@ export default {
       }
       return w / sw
     },
+    rotate(angle) {
+      this.rotation += angle
+      if (this.rotation == 360) {
+        this.rotation = 0
+      }
+      this.group = new fabric.Group(this.myCanvas.getObjects())
+      this.group.rotate(angle)
+      this.myCanvas.centerObject(this.group)
+      this.group.setCoords()
+      this.group.destroy()
+      this.group = null
+      this.myCanvas.renderAll()
+    },
     onClickMoveBtn() {
+      this.destroyGroup()
       this.mode = mode.MOVE
+      this.group = new fabric.Group(this.myCanvas.getObjects())
+      this.group.setCoords()
+      this.group.hasControls = true
+      this.group.hasBorders = true
+      this.group.selectable = true
+      this.myCanvas.setActiveObject(this.group)
+      this.myCanvas.renderAll()
+      console.log('zsf 走了!')
     },
     onClickPenBtn() {
+      this.destroyGroup()
       this.mode = mode.PEN
     },
     onClickEraserBtn() {
+      this.destroyGroup()
       this.mode = mode.ERASER
     },
     onClickRotationBtn() {
-      this.rotation += 90
+      this.destroyGroup()
+      this.rotate(90)
+      this.setMode()
     },
     onClickResetBtn() {
-
+      this.destroyGroup()
+      this.rotate(360 - this.rotation)
+      this.zoomNum = 0
+      this.setZoom()
+      let lines = this.getLines()
+      lines.forEach(item => {
+        this.myCanvas.remove(item)
+      })
+      console.log(this.fabricImg, this.fabricImg.scaleX, this.fabricImg.scaleY, this.fabricImg.x, this.fabricImg.y)
+      this.setMode()
     },
-    onClickBigBtn() {
-
+    onClickZoomInBtn() { // 放大 todo 增加mouse wheel放大 http://fabricjs.com/fabric-intro-part-5
+      this.zoomNum++
+      if (this.zoomNum > this.zoomArr.length - 1) {
+        this.zoomNum = this.zoomArr.length - 1
+      }
+      this.setZoom()
     },
-    onClickSmallBtn() {
-
+    onClickZoomOutBtn() { // 缩小
+      this.zoomNum--
+      if (this.zoomNum < 0) {
+        this.zoomNum = 0
+      }
+      this.setZoom()
     },
     onClickPrevBtn() {
-
+      this.destroyGroup()
     },
     onClickNextBtn() {
-
+      this.destroyGroup()
     },
     onClickHideBtn() {
-
+      this.destroyGroup()
+    },
+    setZoom() {
+      this.destroyGroup()
+      this.group = new fabric.Group(this.myCanvas.getObjects())
+      this.group.setCoords()
+      let targetScale = this.getTargetScaleByGroupWH(this.group.width, this.group.height)
+      this.group.scale(targetScale)
+      this.myCanvas.centerObject(this.group)
+      this.myCanvas.renderAll()
+      this.setMode()
+    },
+    destroyGroup() {
+      if (this.group) {
+        this.myCanvas.discardActiveObject()
+        this.group.selectable = false
+        this.group.destroy()
+        this.group = null
+        this.myCanvas.renderAll()
+      }
+    },
+    getLines() {
+      let lines = []
+      let objs = this.myCanvas.getObjects()
+      objs.forEach(item => {
+        if (item.type == 'path') {
+          lines.push(item)
+        }
+      })
+      return lines
+    },
+    setMode() {
+      if (this.mode == mode.MOVE) {
+        this.onClickMoveBtn()
+      } else if (this.mode == mode.ERASER) {
+        this.onClickEraserBtn()
+      } else if (this.mode == mode.PEN) {
+        this.onClickPenBtn()
+      }
+    },
+    getTargetScaleByGroupWH(groupW, groupH) { // 宽 也可能是高 看旋转方向而定
+      let isCrosswise = this.rotation == 90 || this.rotation == 270 // 横向
+      let defaultWH = isCrosswise ? 750 : 1000
+      let nowWH = isCrosswise ? groupH : groupW
+      let targetZoom = this.zoomArr[this.zoomNum]
+      let targetWH = defaultWH * targetZoom
+      let targetScale = targetWH / nowWH
+      return targetScale
     },
     hi() {
       console.log('app hi')
@@ -110,6 +248,7 @@ export default {
       if (val == mode.PEN) {
         this.myCanvas.isDrawingMode = true
       } else {
+        console.log('zsf 模式变换')
         this.myCanvas.isDrawingMode = false
       }
     }
---
+++ src/tools.js
@@ -0,0 +1,75 @@
+import {isNumber, filter, map, isFinite, isNaN} from 'lodash'
+
+export function getPathStr(pathShape) {
+  var pathData = pathShape.path || []
+  var len = pathData.length
+  var p1 = null
+  var p2 = null
+  var pathArr = filter(pathData, function (item, index) {
+    if (index === 0 || index === len - 1) {
+      return true
+    } else if (index === 1) {
+      p1 = item
+      return true
+    } else if (index === 2) {
+      p2 = item
+      return true
+    }
+
+    var saved = checkCurVature(p1, p2, item)
+
+    if (saved) {
+      p1 = p2
+      p2 = item
+    }
+    return saved
+  })
+
+  pathArr = map(pathArr, function (item, index) {
+    return item.map(function (subItem) {
+      if (isNumber(subItem)) {
+        return Math.round(subItem)
+      }
+      return subItem
+    })
+  })
+
+  len = pathArr.length
+  pathArr = filter(pathArr, function (item, index) {
+    if (index === 0 || index === len - 1) {
+      return true
+    }
+    return index % 2 === 0
+  })
+
+  return pathArr.join(' ').replace(/,/g, ' ')
+}
+
+function checkCurVature(qPoint1, qPoint2, qPoint3) {
+  // 贝塞尔曲线起点与终点的曲率
+  var c1 = (qPoint2[2] - qPoint1[4]) / (qPoint2[1] - qPoint1[3])
+  var c2 = (qPoint3[2] - qPoint2[4]) / (qPoint3[1] - qPoint2[3])
+  var ac1 = Math.abs(c1)
+  var ac2 = Math.abs(c2)
+  var min = Math.max(ac2, ac1)
+
+  if (!isFinite(c1)) {
+    return true
+  }
+
+  if (!isFinite(c2)) {
+    if ((Math.abs(qPoint3[2] - qPoint2[2]) <= 2)) {
+      return false
+    }
+
+    return true
+  }
+
+  var result = Math.abs(c2 - c1) / min
+
+  if (isNaN(result)) {
+    return false
+  }
+
+  return result > 0.12
+}
